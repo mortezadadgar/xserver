fe7703c37fc869a4a639f6460c0780651b735188
mortezadadgar <mortezadadgar97@gmail.com>
2023-10-09 11:32:12 +0330

    Add build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dependecies=(
+	xorg-xkbcomp
+	xorg-setxkbmap
+	xf86-input-libinput
+	xtrans
+	libxfont2
+	xorg-font-util
+	libxcvt
+)
+
+pacman --noconfirm -S "${dependecies[@]}"
+
+arch-meson \
+	--buildtype=release \
+	-Dxorg=true \
+	-Dxdmcp=false \
+	-Dxvfb=false \
+	-Dlinux_acpi=false \
+	-Dhal=false \
+	-Dxnest=false \
+	-Dlinux_apm=false \
+	-Dxkb_dir=/usr/share/X11/xkb \
+	-Dxkb_output_dir=/var/lib/xkb \
+	build
+
+ninja install -C build
