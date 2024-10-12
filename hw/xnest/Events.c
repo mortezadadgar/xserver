pelya/xserver-xsdl

commit 605e6764dfd3e9cb917b9cfcd92fe89857c1a1c9

Fix Motif menu drawing in Xnest.
See also Red Hat bug #229350, OpenSolaris bug #6366490.

hw/xnest/Events.c
@@ -88,7 +88,7 @@ xnestCollectExposures(void)
   while (XCheckIfEvent(xnestDisplay, &X, xnestExposurePredicate, NULL)) {
     pWin = xnestWindowPtr(X.xexpose.window);
     
-    if (pWin) {
+    if (pWin && X.xexpose.width && X.xexpose.height) {
       Box.x1 = pWin->drawable.x + wBorderWidth(pWin) + X.xexpose.x;
       Box.y1 = pWin->drawable.y + wBorderWidth(pWin) + X.xexpose.y;
       Box.x2 = Box.x1 + X.xexpose.width;
@@ -96,7 +96,7 @@ xnestCollectExposures(void)
       
       REGION_INIT(pWin->drawable.pScreen, &Rgn, &Box, 1);
       
-      miWindowExposures(pWin, &Rgn, NullRegion); 
+      miSendExposures(pWin, &Rgn, Box.x2, Box.y2);
     }
   }
 }

mortezadadgar/xserver

commit 9d310679476abb150b8b9055ad44132ffbfa0e3b

Xnest: fix broken exposure events
Xnest fails to properly pass through expose events: the coordinates are
miscalculated in xnestCollectExposures(), before miSendExposures() is called.

Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1735
Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/132
Fixes: 605e6764df - Fix Motif menu drawing in Xnest
Backport-Of: https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1397
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1651>

hw/xnest/Events.c
@@ -94,7 +94,7 @@ xnestCollectExposures(void)
 
             RegionInit(&Rgn, &Box, 1);
 
-            miSendExposures(pWin, &Rgn, Box.x2, Box.y2);
+            miSendExposures(pWin, &Rgn, Box.x1, Box.y1);
         }
     }
 }